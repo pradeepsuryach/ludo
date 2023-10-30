06c8f812cc57439d1844e1a39835ee05cdbf2fa0
y20acs428 <y20acs428@RP21SYS30.win2k18.local>
2023-10-30 11:19:29 +0530

    commit

---
+++ sample/src/main/java/com/dbc/sample/App.java
@@ -0,0 +1,17 @@
+package com.dbc.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int n1=11;
+        int n2=22;
+        int c;
+        c=n1+n2;
+        System.out.println("n1+n2="+c);
+    }
+}
